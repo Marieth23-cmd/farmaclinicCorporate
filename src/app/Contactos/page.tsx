4e7752bf19e11c8cd24e1cdc86ccbfc184cfa1ef
nicosdiogo <nicosdiogojoao@gmail.com>
2026-05-20 09:55:22 +0100

    finalizando o botão solicitar proposta

--- src/app/Contactos/page.tsx
+++ src/app/Contactos/page.tsx
@@ -50,7 +50,7 @@ export default function Contactos() {
 
 
       {/* ── Mobile ──GKIFUNSUNI */}
-          <div className="relative w-full block lg:hidden">
+          <div className="relative mt-20 w-full block lg:hidden">
               <Image
                 src="https://res.cloudinary.com/dhpa1juyr/image/upload/v1778855663/Img_banner_Contacto_phk9b6.webp"
                 alt="Hero"
